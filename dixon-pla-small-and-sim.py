493d03b6004bb19b168fa90c7c2af01d3dbea5da
Paul Sladen <uops@paul.sladen.org>
2020-06-28 19:13:31 +0200

    Further tinkering with AND array decode

--- dixon-pla-small-and-sim.py
+++ dixon-pla-small-and-sim.py
@@ -2,8 +2,22 @@
 
 import operator
 
+# not, using xor to stay postive avoid Python negativity
 def n(number, bits = 104):
     return ((1<<bits)-1)^number
+
+# fixed length bin(), without needing to strip '0b' prefix
+def binstring(number, bits = 24):
+    return ('{:0' + str(bits) + 'b}').format(number)
+
+# iterable bitmask 1, 2, 4, 8, ...
+def bitmask(start = 0x1, bits = None, shift = 1):
+    i = start
+    while bits is None or bits > 0:
+        yield i
+        i = i << shift
+        if bits is not None:
+            bits = bits - 1
 
 def main(instruction = None): 
 
@@ -14,70 +28,56 @@ def main(instruction = None):
     binary_lines = [int(l.rstrip(), 2) for l in lines]
 
     # small decoder: 3 bytes => 24 bits
-    xchg_ecx_eax = 0x910000
     if instruction is None:
+        # fallback example
+        xchg_ecx_eax = 0x910000
         instruction = xchg_ecx_eax
 
-    # is the PLA actually backwards?!
-    flipped = int('{:024b}'.format(instruction)[::-1], 2)
-    #flipped = instruction
-
     # each bit doubled up (if true, if false) => 48 bits
-    x86 = flipped | 0x1000000
-    x86n = x86 ^ 0xffffff | 0x1000000
+    not_x86 = n(instruction,24)
+    x86 = instruction
 
-    # double each bit, flipping the duplicate 
-    z = zip(bin(x86)[3:], bin(x86n)[3:])
+    # double each bit, !bit first
+    z = zip(binstring(not_x86), binstring(x86))
 
-    # + recombine
+    # + interlace bits + convert back from binary-string to binary-number
     pla_input = int(''.join([x for y in z for x in y]),2)
 
-    # pretty print for debugging
-    #print hex(pla_input)
-    #print ' '.join(bin(xchg_ecx_eax)[2:])
-    #print bin(pla_input)[2:]
-
-    pla_and_output = (1 << 104) - 1
-    pla_and_output = 0
-    #print bin(output)
-
-    outputs = []
-
-    active = 0x1 << (48 - 1)
-    #print hex(pla_input)
-    for l in binary_lines:
+    line_outputs = []
+    for l, mask in zip(binary_lines, bitmask()):
         #print hex(active), hex(pla_input), hex(active & pla_input), bool(active & pla_input)
-        if (active & pla_input):
-            #print 'active'
-            outputs.append(l)
+        if (mask & pla_input):
+            line_outputs.append(l)
         else:
-            #outputs.append(0)
             pass
-            
-        #print hex(pla_and_output)
-        #print bin(l)
-        active = active >> 1
-    #print len(outputs)
-    meh = n(0)
-    for o in outputs:
-        #print "{0:#0{1}x}".format(o, 28)
-        # "AND" array is actually NOR...
-        # CHECKME...
-        meh = n(meh) | n(o)
-    
-    print "{0:#0{1}x}".format(instruction, 6+2), 'x86_instruction', '=>',
-    print "{0:#0{1}x}".format(n(meh), 28), 'AND output'
 
+    intermediate = 0
+    for wire in line_outputs:
+        # "AND" array is actually NAND (or is it NOR)...
+        # CHECKME...
+        a = intermediate
+        b = wire
+        intermediate = n(a) & b
+    
+    valid = bool(intermediate & 0x1)
+    prefix = bool(intermediate & 0x2)
+    print "{0:#0{1}x}".format(instruction, 6+2), 'x86_instruction', '=>',
+    print "{0:#0{1}x}".format(intermediate, 28), '(N)AND output',
+    if valid: print '<== valid?',
+    if prefix: print '<== prefix?',
+    print
 
 if __name__=='__main__':
-    for ins in (0x0f0b00, 0x0fff00, 0x0fffff, 0x66f0b0, 0x67f0b0, #Undefined Op-codes
+    for x86 in (0xcc0000, 0xcccc00, 0xcccccc, # Debug interupt
+                0x0f0b00, 0x0fff00, 0x0fffff, 0x66f0b0, 0x67f0b0, #Undefined Op-codes
                 0x909090, 0x669000, 0x0f1f00, 0x0f1f40, 0x660f1f, 0x0f1f80, 0x0f1f84, 0x660f1f, 0x666690, # Nops
                 0x900000, 0x909000, 0x909090, # Repeated Nops, should be the same
                 0x404040, 0x414141, 0x424242, 0x434343, 0x444444, # Inc/Rex single byte
                 0x900000, 0x910000, 0x920000, 0x930000, 0x940000, 0x950000, 0x960000, 0x970000, 0x971234, # xchg
-                0xc8c8c8, 0xc80000, # enter
-                0xc30000, 0xc3c3c3, # ret
-                0x616161, # pushad
+                0xc80000, 0xc80001, # enter
+                0xc30000, 0xc3c3c3, # ret; ret + padding
+                0x600000, 0x606000, 0x606060, # pushad + padding
+                0x610000, 0x616100, 0x616161, 0x666100, # popad
                 0xb80500, 0xb80600, 0xb8ff00, # mov
                 0x48b812, 0xb81200, 0x66b812, 0xb01200, # mov const => rax, eax, ax, al
                 0x31c100, 0x33c800, # two forms of xor eax, ecx
@@ -91,4 +91,4 @@ if __name__=='__main__':
                 0xf7d800, 0xf7d900, # neg reg
                 0x66f7d8, 0x67f7d8, # prefix neg reg
                 ):
-        main(ins)
+        main(x86)
